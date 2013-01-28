2355bbd6680bdf11d8827bdd1dbec97ae9e08ab0
svc.wware.jp <wware@svc.wware.jp>
2013-01-29 00:58:32 +0900

    20130128

--- app/code/community/Openstream/CustomListing/Block/Dropdowned.php
+++ app/code/community/Openstream/CustomListing/Block/Dropdowned.php
@@ -5,17 +5,19 @@ class Openstream_CustomListing_Block_Dropdowned extends Openstream_CustomListing
 // $attribute_value
     protected function _getProductCollection()
     {
-	    error_log("Openstream_CustomListing_Block_Dropdowned:" . print_r($attribute_value, true));
+	    
 
         if (($attribute_code = $this->getAttribute()) && ($attribute_value = $this->getValue())) {
             if (is_null($this->_productCollection)) {
                 $this->_productCollection = Mage::getResourceModel('reports/product_collection');
                 $this->_productCollection->addAttributeToFilter($attribute_code, array('neq' => ''))
-//                						 ->addAttributeToSort($attribute_code, "desc")
-                                         ->addAttributeToSelect('*')
+                						 ->addAttributeToSelect('*')
                                          ->addStoreFilter();
+
             }
         }
+//        kint::dump($this->_productCollection);exit;
+		$this->_customlistingToolbarDisable = true;
         return $this->_productCollection;
     }
 }
