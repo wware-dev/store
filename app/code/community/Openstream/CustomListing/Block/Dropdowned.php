3d2218fcaf99da7eeba1e37ca99d8b41a445d0f9
svc.wware.jp <wware@svc.wware.jp>
2013-01-28 19:54:52 +0900

    20130128

--- app/code/community/Openstream/CustomListing/Block/Dropdowned.php
+++ app/code/community/Openstream/CustomListing/Block/Dropdowned.php
@@ -5,9 +5,10 @@ class Openstream_CustomListing_Block_Dropdowned extends Openstream_CustomListing
 // $attribute_value
     protected function _getProductCollection()
     {
+	    error_log("Openstream_CustomListing_Block_Dropdowned:" . print_r($attribute_value, true));
+
         if (($attribute_code = $this->getAttribute()) && ($attribute_value = $this->getValue())) {
             if (is_null($this->_productCollection)) {
-            	error_log($attribute_code);
                 $this->_productCollection = Mage::getResourceModel('reports/product_collection');
                 $this->_productCollection->addAttributeToFilter($attribute_code, array('neq' => ''))
 //                						 ->addAttributeToSort($attribute_code, "desc")
